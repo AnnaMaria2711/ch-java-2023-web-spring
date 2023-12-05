d834795db09850f29a8e37b7a70ac710bdebec73
AnnaMaria2711 <Asnna-Maria.Buelk@coop.ch>
2023-12-05 16:42:25 +0100

    ch-java-2023-web-spring and Git created

---
+++ src/main/java/com/example/chjava2023webspring/ChJava2023WebSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023webspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChJava2023WebSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChJava2023WebSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chjava2023webspring/ChJava2023WebSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chjava2023webspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChJava2023WebSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
